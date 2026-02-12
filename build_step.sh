5a0ed4b8d8c181aa4d2c0dfb92245054392a8ddb
aalokor <aalo.korkeaaho@gmail.com>
2026-02-12 20:02:18 +0200

    modify build_step.sh

--- build_step.sh
+++ build_step.sh
@@ -6,12 +6,12 @@ echo "Starting production build"
 
 # Backend build
 echo "Building backend..."
-npm install --production
+npm install
 npm run tsc
 
 # Frontend build
 echo "Building frontend..."
-(cd frontend && npm install --production && npm run build)
+(cd frontend && npm install && npm run build)
 
 echo "Build finished successfully"
 
