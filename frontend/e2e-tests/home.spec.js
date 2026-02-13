415edf3e3bb371c2a64510aed021b3fb5b3ac0a7
aalokor <aalo.korkeaaho@gmail.com>
2026-02-13 13:50:20 +0200

    fix playwright

--- frontend/e2e-tests/home.spec.js
+++ frontend/e2e-tests/home.spec.js
@@ -1,10 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:5173";
-
 test.describe("Home", () => {
   test("front page can be opened", async ({ page }) => {
-    await page.goto(BASE_URL);
+    await page.goto("");
     await expect(page.getByText("Patient list")).toBeVisible();
   });
 });
